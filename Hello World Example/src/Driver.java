9b1f261dbad5b03aba0469e6e5b6f8b166c41d35
Ben Z <bigbenelectronicsco@gmail.com>
2015-11-09 23:09:47 -0700

    Added an example java project.

---
+++ Hello World Example/src/Driver.java
@@ -0,0 +1,8 @@
+//Made By Ben
+import javax.swing.*;
+public class Driver {
+	public static void main( String args[] )
+	{
+		JOptionPane.showMessageDialog(null, "Test Message", "Test program", JOptionPane.WARNING_MESSAGE);
+	}
+}
