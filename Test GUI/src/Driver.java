ef2b096c62f88a3b137cd3b77cd231be41ec16c9
Venancio (Ben) T Zuniga <bigbenelectronicsco@gmail.com>
2015-11-12 16:44:06 -0700

    Started getting the idea of it together during lunch.

--- Test GUI/src/Driver.java
+++ Test GUI/src/Driver.java
@@ -7,3 +7,12 @@ public class Driver
     
   }
 }
+
+
+/*
+ * Notes:
+ * I think the way I am going to organize the code is by making a class for a 
+ * Particular object I want, for example a panel for the Buttons, another class 
+ * for the panel that holds the tool bar. In those classes will be the code for the layout.
+ * 
+ * */
